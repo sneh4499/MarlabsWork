06530f8c589b1cdf7e20dd211f0c94367dc3d22a
Snehal <psnehal.er@gmail.com>
2023-10-02 23:30:18 -0400

    oct2

---
+++ ConsoleApp1/Oct2/BackgroundThreadsEx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Oct2
+{
+    internal class BackgroundThreadsEx
+    {
+        static void BackgroundWork()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("This is Background thread running....");
+            }
+        }
+
+        static void ForegroundWork()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("This is Foreground thread running....");
+            }
+        }
+
+        static void Main()
+        {
+            Thread th1 = new Thread(BackgroundWork);
+            th1.IsBackground = true;
+            th1.Start();
+
+            Thread th2 = new Thread(ForegroundWork);
+            th2.Start();
+
+            for(int i = 0;i < 2;i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("This is main thread running on...");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Oct2/ThreadPoolEx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1.Oct2
+{
+    internal class ThreadPoolEx
+    {
+        static void Main()
+        {
+            // Queue some tasks to the thread pool
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(DoWork, i);
+            }
+
+            // Wait for the tasks to complete (just for demonstration purposes)
+            Thread.Sleep(1000);
+
+            Console.WriteLine("All tasks have completed.");
+        }
+
+        static void DoWork(object state)
+        {
+            int taskId = (int)state;
+            Console.WriteLine($"Task {taskId} is running on Thread {Thread.CurrentThread.ManagedThreadId}");
+            // Perform some work here
+        }
+    }
+}
